c61820df841833d36bd628b40874a72ba4008db7
ano-op <58827492+ano-op@users.noreply.github.com>
2021-04-15 13:08:04 -0400

    Update mod.js

--- src/http/get-index/mod.js
+++ src/http/get-index/mod.js
@@ -11,7 +11,7 @@ export async function handler (/*req: object*/) {
       /**
        * Basic bio
        */
-      fullname: 'Your Name', // ←  Start by adding your name!
+      fullname: 'Anoop Kumar', // ←  Start by adding your name!
       title: 'My personal site!',
       occupation: 'Artist & Photographer',
       location: 'West Glacier, MT',
